2b6f34e14fedc591086a8e45ce256cd43cdbce86
gcbeen <johngcb@163.com>
2022-03-11 16:40:38 +0800

    add packages

--- routes/mysql.js
+++ routes/mysql.js
@@ -1,34 +1,39 @@
-let mysql = require('mysql')
+let mysql = require("mysql");
 
 let sqlOptions = {
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: '112233',
-  database: 'supermall'
-}
+  // host: "localhost",
+  // port: "3306",
+  // user: "root",
+  // password: "JavaScriptGO@123456",
+  // database: "mallPro",
+  host: "db4free.net",
+  port: "3306",
+  user: "gcbeen",
+  password: "JavaScriptGO@123456",
+  database: "mall_pro",
+};
 
 // 创建连接
 
-let con = mysql.createConnection(sqlOptions)
-con.connect(err=>{
-  if(err){
-    console.log('错误信息：'+err);
-  }else{
-    console.log('数据库连接成功');
+let con = mysql.createConnection(sqlOptions);
+con.connect((err) => {
+  if (err) {
+    console.log("错误信息：" + err);
+  } else {
+    console.log("数据库连接成功");
   }
 });
 
-function sqlQuery(strSql,arr){
-  return new Promise((resolve,reject) =>{
-    con.query(strSql,arr,(err,results)=>{
-      if(err){
-        reject('错误信息：'+err)
-      }else{
-        resolve(results)
+function sqlQuery(strSql, arr) {
+  return new Promise((resolve, reject) => {
+    con.query(strSql, arr, (err, results) => {
+      if (err) {
+        reject("错误信息：" + err);
+      } else {
+        resolve(results);
       }
-    })
-  })
+    });
+  });
 }
 
-module.exports = sqlQuery
+module.exports = sqlQuery;
